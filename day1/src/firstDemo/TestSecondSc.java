5fd9c23749f95c8677b86bb9e3ece19736385c3e
akkie00 <akshay8801.ak@gmail.com>
2020-06-08 20:47:51 +0530

    modifed the testClass

--- day1/src/firstDemo/TestSecondSc.java
+++ day1/src/firstDemo/TestSecondSc.java
@@ -1,5 +1,6 @@
 package firstDemo;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TestSecondSc {
@@ -8,15 +9,30 @@ public class TestSecondSc {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.US);
 		System.out.println("Enter values: ");
-		Second avg = new Second(sc.nextDouble(), sc.nextDouble());
 		if (sc.hasNextDouble()) {
-//			System.out.println(sc.hasNextDouble());
-			System.out.println("Addition is: " + avg.getAddition());
-			System.out.println("Average is: "+avg.getAverage());
-		} else {
-			System.out.println("enter only double value: " + sc.hasNextDouble());
+//				Second avg = new Second(sc.nextDouble(), sc.nextDouble());
+//			Second avg = new Second(num1, num2);
+				double d1 = sc.nextDouble();
+				 if(sc.hasNextDouble()){
+				double d2 = sc.nextDouble();
+				Second avg = new Second(d1, d2);
+//				System.out.println("value of if" + sc.hasNextDouble());
+//				System.out.println("Addition is: " + avg.getAddition());
+				System.out.println("Average is: " + avg.getAverage());
+
+			}
+				 else if(sc.hasNext()) {
+						System.out.println("enter only double value: " + sc.hasNextDouble());
+					}
 		}
+			
+		else {
+			System.out.println("enter only double");
+		}
+		
+		sc.close();
 	}
 
 }
